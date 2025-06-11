5649b1a77958fda400b9eec6e5e47859250f0922
amrit poudel <aamreet12345@gmail.com>
2025-06-11 07:56:35 +0545

    day:2 variables

---
+++ day1/variables.go
@@ -0,0 +1,17 @@
+package variables
+
+import "fmt"
+
+func main() {
+
+	//here i will define variables then use
+	var name string = "amrit"
+	age := 26
+	var height float64 = 5.7
+	is_learning_golang := true
+	fmt.Println("my name is ", name)
+	fmt.Println("i am ", age, "years old")
+	fmt.Println("i am ", height, "ft tall")
+	fmt.Println("am i learning golang: ", is_learning_golang)
+
+}
