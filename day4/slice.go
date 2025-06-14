50c515f1a94ccd4087513a276959265c95566f0b
amrit poudel <aamreet12345@gmail.com>
2025-06-14 22:45:55 +0545

    slice done

--- day4/slice.go
+++ day4/slice.go
@@ -1,4 +1,6 @@
-package main
+package slice_practice
+
+import "fmt"
 
 func main() {
 	// arr := [5]int{1, 2, 3, 4, 5}
@@ -11,5 +13,37 @@ func main() {
 	// slice1[2] = 40
 	// fmt.Println(len(arr))
 	// fmt.Println(arr)
+	// slices are nothing but array that point to original array it holds pointer to array,length current and total capacity
+	//define slice
+	// amritarr := [10]int{54, 65, 12, 76, 89, 2, 3, 12, 34, 65}
+	// fmt.Println("original array: ", amritarr)
+	// amritslice := amritarr[2:6]
+	// fmt.Println("original slice: ", amritslice)
+	// //making new slice
+	// newslice := make([]int, 5)
+	// copy(newslice, amritslice)
+	// fmt.Println("dynamically created slice: ", newslice)
+	// //lets change in slice then see difference in original slice we changed item in slice but original data
+	// //changed because slice was just name or reference to original array so changes in one result change in another.
+
+	// amritslice[2] = 1
+	// fmt.Println("original slice: ", amritslice)
+	// fmt.Println("original array: ", amritarr)
+
+	// practice question
+	// 1)
+	// 	Create a slice of 5 ints. Print len and cap.
+	// Append two more values. Print updated len and cap.
+	// Slice [1:4] from an array. Print and modify it. Show array impact.
+	amritarr := [10]int{1, 4, 2, 6, 3, 56, 1, 5, 6, 1}
+	slice1 := amritarr[0:5]
+	fmt.Println("slice : ", slice1)
+	fmt.Println("length: ", len(slice1))
+	fmt.Println("cap: ", cap(slice1))
+
+	slice1 = append(slice1, 2, 3)
+	fmt.Println("slice : ", slice1)
+	fmt.Println("length: ", len(slice1))
+	fmt.Println("cap: ", cap(slice1))
 
 }
