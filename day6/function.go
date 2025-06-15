87d8d910a56eda396cf72eaf30bf73ecfc0bb6ab
amrit poudel <aamreet12345@gmail.com>
2025-06-15 20:13:18 +0545

    function1 done

---
+++ day6/function.go
@@ -0,0 +1,30 @@
+package main
+
+func main() {
+	//1.Write a function calc that takes two integers and returns their sum, difference, product, and quotient.
+	// get input from user
+	// var a, b int
+	// fmt.Printf("input first number: ")
+	// fmt.Scan(&a)
+	// fmt.Printf("input second number: ")
+	// fmt.Scan(&b)
+	// sum, difference, product, quotent := calc(a, b)
+	// fmt.Printf("sum: %d \n difference: %d \n product: %d \n quotent: %d", sum, difference, product, quotent)
+
+	//2.Write a function that takes any number of integers and returns their total sum.
+	// fmt.Println("the total sum of numbers is : ", infinite_sum(2, 3, 4, 5, 6, 7, 8, 9))
+	//3.
+
+}
+
+// func calc(a int, b int) (int, int, int, int) {
+// 	return a + b, a - b, a * b, a / b
+// }
+
+// func infinite_sum(numbers ...int) int {
+// 	sum := 0
+// 	for _, number := range numbers {
+// 		sum = sum + number
+// 	}
+// 	return sum
+// }
