8772b6126488cc63b13e667ebb4e175ef6930e69
amrit poudel <aamreet12345@gmail.com>
2025-06-18 22:28:28 +0545

    nasdlf

---
+++ day8/pointer.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 20
+	var ptr *int
+	var ptr1 *int
+	ptr = &a
+	fmt.Println("the pointer pointing value is ", *ptr)
+	fmt.Println("actual pointer valueof address it pointting to is ", ptr)
+	fmt.Println("address of a ", &a)
+	increment(ptr)
+	fmt.Println(a)
+	fmt.Println("here we are seeing what empty pointer hold ")
+	if ptr1 == nil {
+		fmt.Println("it is pointing nill")
+	}
+}
+
+func increment(temp *int) {
+	*temp++
+}
