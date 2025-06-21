39b59a30003865ee9e5fa54b76a94d2dbf2b4473
amrit poudel <aamreet12345@gmail.com>
2025-06-21 21:39:32 +0545

    struct and old

--- day10/structure.go
+++ day10/structure.go
@@ -1,5 +1,38 @@
-package main
+package struct1
+
+import "fmt"
 
 func main() {
+
+	// type person struct {
+	// 	fname string
+	// 	lname string
+	// 	age   int
+	// }
+	// // creating objects of struct
+	// p1 := person{
+	// 	fname: "amrit",
+	// 	lname: "poudel",
+	// 	age:   26,
+	// }
+	// p2 := person{
+	// 	fname: "anita",
+	// 	lname: "poude;",
+	// 	age:   23,
+	// }
+	// fmt.Println(p1.fname, p1.lname, p1.age)
+	// fmt.Println(p2)
+
+	// we also can use anynomous struct for temporary purpose
+	user := struct {
+		fname string
+		lname string
+		age   int
+	}{
+		fname: "amrit",
+		lname: "poudel",
+		age:   20,
+	}
+	fmt.Println(user)
 
 }
