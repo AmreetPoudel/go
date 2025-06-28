d8ef2402399f8663e98282e4fa135c7cfcce7e36
amrit poudel <aamreet12345@gmail.com>
2025-06-28 11:36:51 +0545

    error 1

---
+++ erros.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func SquareRoot(number float64) (float64, error) {
+	if number < 0 {
+		return 0, errors.New("math: cannot compute squareroot of negative number")
+	} else {
+		return math.Sqrt(number), nil
+	}
+
+}
+
+func main() {
+	number := -11
+	n, e := SquareRoot(float64(number))
+	if e != nil {
+		fmt.Println(e)
+	} else {
+		fmt.Printf("The square root of given number: %v is %.2f\n", number, n)
+	}
+
+}
